3c44616dbc68b3a274c3173d60ff3a86d54f7129
ganny8 <gannyboychoy@gmail.com>
2025-08-27 09:48:50 +0800

    Firebase Config

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDfJ4sDN2QjQI_-0y5O7OFSshAMXqsfbec",
+  authDomain: "jntr-goals.firebaseapp.com",
+  projectId: "jntr-goals",
+  storageBucket: "jntr-goals.firebasestorage.app",
+  messagingSenderId: "187745158707",
+  appId: "1:187745158707:web:b146f768a903f73f508546"
+};
+
+const app = initializeApp(firebaseConfig);

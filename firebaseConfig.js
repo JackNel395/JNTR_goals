7fe2aeeae548df890890cefb61ccbb46c79aa861
JackNel395 <jnrebias@gmail.com>
2025-09-01 13:02:14 +0800

    Create Goals

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDfJ4sDN2QjQI_-0y5O7OFSshAMXqsfbec",
-  authDomain: "jntr-goals.firebaseapp.com",
-  projectId: "jntr-goals",
-  storageBucket: "jntr-goals.firebasestorage.app",
-  messagingSenderId: "187745158707",
-  appId: "1:187745158707:web:b146f768a903f73f508546"
+  apiKey: "AIzaSyBAgfQTe570bjh8Z8OrYWrP0PRoKe8qP30",
+  authDomain: "jntr-goal.firebaseapp.com",
+  projectId: "jntr-goal",
+  storageBucket: "jntr-goal.firebasestorage.app",
+  messagingSenderId: "119524408487",
+  appId: "1:119524408487:web:bebca78197da86ef632f82"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-//yo
